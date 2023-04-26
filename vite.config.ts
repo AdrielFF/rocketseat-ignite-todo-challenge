727e41bae69eeebf2323d0f39c4924b8277d306a
AdrielFF <adrielfernandes321@gmail.com>
2023-04-25 21:28:27 -0300

    remove dist folder from main branch

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "{repName}",
+  base: "/rocketseat-ignite-todo-challenge/",
   plugins: [react()],
 });
